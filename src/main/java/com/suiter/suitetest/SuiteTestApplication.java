a45d9ab524937303f9b6d716746fbde17682c6cb
mjung1798 <mor2222@naver.com>
2020-01-14 22:05:58 +0900

    project name edit

---
+++ src/main/java/com/suiter/suitetest/SuiteTestApplication.java
@@ -0,0 +1,13 @@
+package com.suiter.suitetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiteTestApplication.class, args);
+	}
+
+}
--- src/test/java/com/suiter/suitetest/SuiteTestApplicationTests.java
+++ src/test/java/com/suiter/suitetest/SuiteTestApplicationTests.java
@@ -1,13 +1,13 @@
-package com.jyami.suittest;
+package com.suiter.suitetest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SuitTestApplicationTests {
+class SuiteTestApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
